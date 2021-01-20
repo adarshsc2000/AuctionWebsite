5e74cebc4bc98249dc965eb89221c92d22c799bf
AdarshChandran2000 <lachikku2210@gmail.com>
2021-01-20 14:30:24 +0300

    Updated project_connection.php

--- project_connection.php
+++ project_connection.php
@@ -1,7 +1,6 @@
 <?php
 
-
-$db=new PDO('mysql:host=sql204.epizy.com;dbname=epiz_27510450_itcs333project_group4;charset=utf8','epiz_27510450','mgvj6V1KmcUB');
+  $db=new PDO('mysql:host=localhost;dbname=epiz_27510450_itcs333project_group4;charset=utf8','root','');
 	$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-	
+
 	?>
