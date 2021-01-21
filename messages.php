b85198bb1e61000d45553102209d7f82697a5554
AdarshChandran2000 <lachikku2210@gmail.com>
2021-01-21 14:29:58 +0300

    messages.php UPDATED Changes in design. Added dp. Added send button.

--- messages.php
+++ messages.php
@@ -5,12 +5,35 @@ $sid = $_SESSION['userId'];
 try {
         require('project_connection.php');
         $sql="SELECT * FROM auctions WHERE (OWNER_ID = $sid OR WINNER_ID = $sid) AND STATUS is not null";
-        $sql1="SELECT USERNAME FROM users WHERE USER_ID =?";
+        $sql1="SELECT USERNAME, PROFILE_PIC FROM users WHERE USER_ID =?";
         $msg1 = $db->prepare($sql1);
         $rs=$db->query($sql);
         ?>
         <html>
         <head>
+          <style>
+            .table
+            {
+              border: 20px solid white;
+            }
+            tr
+            {
+              border: 10px solid white;
+            }
+            .send
+            {
+              font-weight: 1000;
+              border: 5px solid black;
+              background-color: #050505;
+              color: white;
+            }
+            .send:hover
+            {
+              background-color: #3687f4;
+              color: #050505;;
+            }
+          </style>
+        </head>
         <body>
           <header class="masthead bg-primary text-white text-center px-md-2">
               <div class="container d-flex align-items-center flex-column">
@@ -24,8 +47,8 @@ try {
                   </div>
               </div>
           </header>
-          <div class='container'>
-        <table class='table table-striped table-primary'>
+          <div>
+         <table class='table table-striped bg-primary text-white'>
           <?php
           while($row = $rs->fetch())
           {
@@ -37,8 +60,18 @@ try {
                 echo "<tr>";
                 $msg1->execute(array($person_id));
                 if($username = $msg1->fetch())
-                  echo "<td><h2>".$username[0].": ".$row["AUCTION_NAME"]."</h2></td>";  // name is the first element so [0]
-                echo "<td><a href='chat.php?id=". $row['AUCTION_ID']."&rid=".$person_id."'> <img src='images/message.png' width='50px' height='50px' > </a></td>";
+                  {
+                    echo "<td><img src='uploadedfiles/".$username[1]."' width='100px' height='100px' ></td>";
+                    echo "<td><h2>".$username[0].": "."</h2></td>";  // name is the first element so [0]
+                  }
+                echo "<td><h2>".$row["AUCTION_NAME"]."</h2></td>";
+                echo "<td><h2>";
+                  echo "<form method='get' action='chat.php'>";
+                    echo "<input hidden name='id' value='".$row['AUCTION_ID']."'/>";
+                    echo "<input hidden name='rid' value='".$person_id."'/>";
+                    echo "<input class='btn btn-outline-secondary my-4 my-lg-3 btn-lg send' type='submit' value='MESSAGE'/>";
+                  echo "</form>";
+                echo "</h2></td>";
                 echo "</tr>";
 
             echo "<br>";
@@ -46,6 +79,7 @@ try {
           ?>
           </table>
         </div>
+          </br>
           </body>
           </html>
         <?php
